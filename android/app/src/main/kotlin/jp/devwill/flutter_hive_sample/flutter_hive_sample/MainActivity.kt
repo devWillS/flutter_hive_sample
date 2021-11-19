14b72db6e4796cda98366450d6500d2a24345191
devWillS <>
2021-11-19 20:44:11 +0900

    first commit

---
+++ android/app/src/main/kotlin/jp/devwill/flutter_hive_sample/flutter_hive_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package jp.devwill.flutter_hive_sample.flutter_hive_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hive_sample/views/my_home_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() async {
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
